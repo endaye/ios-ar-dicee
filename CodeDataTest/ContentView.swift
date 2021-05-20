8c70e93f4368c654effb32b2a41799490c04cda1
Yuancheng Zhang <old.far.hot@gmail.com>
2021-05-20 21:59:11 +0800

    done #368

---
+++ CodeDataTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeDataTest
+//
+//  Created by Yuancheng Zhang on 2021/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

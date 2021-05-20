335a6b07d5052f708e0638eb1296791dc5e7ddcc
Yuancheng Zhang <old.far.hot@gmail.com>
2021-05-20 22:01:06 +0800

    done #368

---
+++ ARDicee/Controllers/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Yuancheng Zhang on 2021/5/20.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+     
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let sphere = SCNSphere(radius: 0.2)
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+//        sphere.materials = [material]
+//
+//        let node = SCNNode()
+//        node.position = SCNVector3(0, 0.1, -0.5)
+//        node.geometry = sphere
+//        sceneView.scene.rootNode.addChildNode(node)
+        
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0.0, y: 0.0, z: -0.1)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+  
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        print("Session is supported = \(ARConfiguration.isSupported)")
+        print("World Tracking is supported = \(ARWorldTrackingConfiguration.isSupported)")
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
